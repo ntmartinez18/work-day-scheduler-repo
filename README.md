99fceb903cac93cdcdede9ce74bf5847449072a8
taylor.martinez <taylor.swot.2018@gmail.com>
2023-09-08 08:32:18 -0500

    updated script.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # work-day-scheduler-repo
+
+## A daily scheduler for the work day hours of 9am-5pm.
+
+## Description 
+Provides users a daily planner to schedule events from the standrd business hours of 9am to 5pm. Allows users to view past, present, and future hours with different styling classes to provide a clear view of the current hour and future and past hours along with those events.
--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+// created global variables to be accessed later
+const saveButton = document.querySelector(".saveBtn");
+var timeBlock = document.querySelector(".time-block");
+var userHour = document.getElementById("currentDay");
 
+var userInput;
 
 
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
 
-
-
-  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -29,7 +32,31 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 
+  // created function to compare if userhour was the same as the hour timeblock and then update style accordingly
+  function updateHour() {
+    if (timeBlock === userHour) {
+      $(timeBlock).removeClass("future");
+      $(timeBlock).removeClass("past");
+      $(timeBlock).addClass("present");
+    };  else if (timeBlock < userHour) {
+      $(timeBlock).removeClass("future");
+      $(timeBlock).removeClass("present");
+      $(timeBlock).addClass("past");
+    };  else (timeBlock > userHour) {
+      $(timeBlock).removeClass("past");
+      $(timeBlock).removeClass("present");
+      $(timeBlock).addClass("future");
+    };
+  };
+
+  // created function to update tasks in time blocks
+  function updateTask() {
+    localStorage.setItem(userInput);
+  };
+
 
 
 
 });
+
+saveButton.addEventListener("click", updateTask)
